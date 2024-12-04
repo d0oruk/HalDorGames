039a0cc991748f98a44d9cf7dee1d2feceae5569
d0oruk <dorukdemirbilek@gmail.com>
2024-12-04 15:13:29 +0300

    Add project files.

---
+++ BLL/Controllers/Bases/MvcController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Controllers.Bases
+{
+    public abstract class MvcController : Controller
+    {
+        protected MvcController()
+        {
+            //THIS CODE BLOCK SHOULD BE IN CONSTURCTOR!
+            CultureInfo culterInfo = new CultureInfo("tr-TR"); //en-US
+            Thread.CurrentThread.CurrentCulture = culterInfo;
+            Thread.CurrentThread.CurrentUICulture = culterInfo;
+        }
+
+
+    }
+}
